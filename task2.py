c37da4241e7048ecdce49e2595de20217a5af079
Egor <1begosha@gmail.com>
2022-12-12 21:51:24 +0300

    fixed

--- task2.py
+++ task2.py
@@ -9,25 +9,19 @@ def randomize(input_list, qtys):
     """Функция для создания массива со случайными целыми числами из пустого входящего массива.
     На входе ожидается пустой массив и количество элементов, которое должно быть сгенерировано
     """
-    i = 0
-    qtys = int(qtys)
-    input_list = []
-    while i < qtys:
-        input_list.append(rnd(0, 10))
-        i += 1
+    input_list = [rnd(0,10) for x in range(qtys)]
     return input_list
+    
     
 def mult_elem (input_list):
     """Функция возвращает массив из значений, где каждое значение = входящиймассив[индекс]*входящиймассив[размер массива - индекс]
     На входе ожидает массив"""
-    i=0
     multiplied_list = []
     for position,elem in enumerate(input_list[0:len(input_list)//2]):
         multiplied_list.append(elem*input_list[len(input_list)-position-1])
-        i+=1
     return multiplied_list
 
-qty = input('Введите количество элементов массива: ')
+qty = int(input('Введите количество элементов массива: '))
 pure_list = []
 pure_list = randomize(pure_list,qty)
 print(pure_list)
